8a143ef57bf8bd3f59b3542a49f05c5e47593f34
Harry Ross <harry.ross@xero.com>
2015-06-17 12:02:41 +1200

    different file path

--- CodeImpact/Program.cs
+++ CodeImpact/Program.cs
@@ -7,7 +7,7 @@ namespace CodeImpact
         static void Main(string[] args)
         {
             var method = new PopulateDirectoryController();
-            method.PopulateDictionary("D:\\Dev\\SImplePrintApp");
+            method.PopulateDictionary("C:\\Dev\\MyXero\\project\\MyXeroWeb");
         }
     }
 
